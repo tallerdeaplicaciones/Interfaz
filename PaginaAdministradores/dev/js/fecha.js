f85b7051c8fce84f59b687d1fd39ae3b04af6086
Seba-perseverance <scastrof2@outlook.com>
2023-06-23 02:06:21 -0400

    jueves2

--- PaginaAdministradores/dev/js/fecha.js
+++ PaginaAdministradores/dev/js/fecha.js
@@ -3,3 +3,36 @@ var dia = fecha.getDate();
 var mes = fecha.getMonth()+1;
 var anio = fecha.getFullYear();
 document.getElementById("fechaActual").innerHTML = `${dia}/${mes}/${anio}`
+function sel(){
+    var filtroSelect = document.getElementById('documento').value;
+    var filas = document.querySelectorAll(' #tabla-ventas tbody');
+
+    if (filtroSelect === '') {
+        mostrarFilas(filas);
+    } else {
+        ocultarFilas(filas);
+        mostrarFilasPorTipo(filas, filtroSelect);
+    }
+}
+
+function ocultarFilas(filas) {
+    for (var i = 0; i < filas.length; i++) {
+        filas[i].style.display = 'none';
+    }
+}
+
+function mostrarFilas(filas) {
+    for (var i = 0; i < filas.length; i++) {
+        filas[i].style.display = 'table-row';
+    }
+}
+
+function mostrarFilasPorTipo(filas, filtroSelect) {
+    for (var i = 0; i < filas.length; i++) {
+        var tipo = filas[i].querySelector('td:first-child').textContent;
+
+        if (tipo === filtroSelect) {
+            filas[i].style.display = 'table-row';
+        }
+    }
+}
